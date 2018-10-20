5f6dc61f32a7a0824b8164a690a3fedb536f9640
Michael Verges <michaelverges@lawn-143-215-60-143.lawn.gatech.edu>
2018-10-19 22:30:46 -0400

    initial commit

---
+++ IRIS iOS/IRIS iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IRIS iOS
+//
+//  Created by Michael Verges on 10/19/18.
+//  Copyright © 2018 Michael Verges. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ IRIS/IRIS/IRIS.h
@@ -0,0 +1,19 @@
+//
+//  IRIS.h
+//  IRIS
+//
+//  Created by Michael Verges on 10/19/18.
+//  Copyright © 2018 Michael Verges. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IRIS.
+FOUNDATION_EXPORT double IRISVersionNumber;
+
+//! Project version string for IRIS.
+FOUNDATION_EXPORT const unsigned char IRISVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRIS/PublicHeader.h>
+
+
