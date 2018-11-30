274124f3bea074a5213cfaf55c975db5e94a82ef
Max Zuo <max.zuo@gmail.com>
2018-11-30 05:17:17 +0000

    Added necessary config function to git_tools. uploading .m5 files to git repo now possible

--- git_tools.py
+++ git_tools.py
@@ -2,19 +2,20 @@ import os
 import sys
 
 def commit(message="", push=True):
-	os.sys('git commit -m "' + message + '"')
-	if (push): os.sys('git push -u')
+	os.system('git commit -m "' + message + '"')
+	if (push): os.system('git push -u')
 
 def add(filename):
-	os.sys('git add ' + filename)
+	os.system('git add ' + filename)
 
 
-
+def config(username, password, repo_url):
+	os.system('git config remote.origin.url https://{}:{}@{}'.format(username, password, repo_url.split("//")[1]))
 
 
 
 if __name__ == "__main__":
 	if len(sys.argv) <= 1:
-		return
+		sys.exit(1)
 	message = sys.argv[1]
 	commit(message)
